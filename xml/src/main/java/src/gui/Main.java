162c979232d56771af1d58d8a4be58ec3f0e2953
Wiktor <wiktor.muraszko@wp.pl>
2019-06-13 23:02:53 +0200

    Dwie listy

--- xml/src/main/java/src/gui/Main.java
+++ xml/src/main/java/src/gui/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
         System.out.println(getClass().getResource("/sample.fxml"));
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
