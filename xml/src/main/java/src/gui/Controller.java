251bb53617ae1a46895106ecc84641f2aef8617c
Wiktor <wiktor.muraszko@wp.pl>
2019-06-13 23:15:17 +0200

    gui v2

--- xml/src/main/java/src/gui/Controller.java
+++ xml/src/main/java/src/gui/Controller.java
@@ -88,6 +88,7 @@ public class Controller implements Initializable {
         }
         root = XMLOperations.root;
         refreshListaPlaylist();
+        refreshListaWykonawcow();
     }
 
     public void refreshListaGatunkow() {
